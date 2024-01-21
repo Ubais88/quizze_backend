d875ac8e078a65106a8bb77a8061ef5dfb77fce5
Ubais88 <moubais2143@gmail.com>
2024-01-21 22:51:13 +0530

    user login route and controller added

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -5,21 +5,11 @@ require("dotenv").config();
 // Signup Controller for Registering USers
 exports.signup = async (req, res) => {
   try {
-
     // Destructure fields from the request body
-    const {
-      name,
-      email,
-      password,
-      confirmPassword,
-    } = req.body;
-    
+    const { name, email, password, confirmPassword } = req.body;
+
     // Check if All Details are there or not
-    if (
-      !name ||
-      !email ||
-      !password
-    ) {
+    if (!name || !email || !password) {
       return res.status(403).send({
         success: false,
         message: "All Fields are required",
@@ -62,6 +52,56 @@ exports.signup = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "User cannot be registered. Please try again.",
+    });
+  }
+};
+
+// Login controller for authenticating users
+exports.login = async (req, res) => {
+  try {
+    // Get email and password from request body
+    const { email, password } = req.body;
+
+    // Check if email or password is missing
+    if (!email || !password) {
+      // Return 400 Bad Request status code with error message
+      return res.status(400).json({
+        success: false,
+        message: `Please Fill up All the Required Fields`,
+      });
+    }
+
+    // Find user with provided email
+    const user = await User.findOne({ email });
+
+    // If user not found with provided email
+    if (!user) {
+      // Return 401 Unauthorized status code with error message
+      return res.status(401).json({
+        success: false,
+        message: `User is not Registered with Us Please SignUp to Continue`,
+      });
+    }
+
+    //Compare Password
+    if (await bcrypt.compare(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        user,
+        message: `User Login Success`,
+      });
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: `Password is incorrect`,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    // Return 500 Internal Server Error status code with error message
+    return res.status(500).json({
+      success: false,
+      message: `Login Failure Please Try Again`,
     });
   }
 };
--- routes/User.js
+++ routes/User.js
@@ -3,12 +3,13 @@ const express = require("express");
 const router = express.Router();
 
 // Import the required controllers and middleware functions
-const { signup } = require("../controllers/Auth");
+const { signup, login } = require("../controllers/Auth");
 
 
 // Route for user signup
 router.post("/signup", signup)
-
+// Route for user login
+router.post("/login", login)
 
 // Export the router for use in the main application
 module.exports = router;
